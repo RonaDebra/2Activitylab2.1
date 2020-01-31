daa7f72e0c28446cd8e5b040281b456830a7e89d
RonaDebra <52830917+RonaDebra@users.noreply.github.com>
2020-01-31 22:18:58 +0300

    Initial commit

---
+++ app/src/main/java/android/example1/com/MainActivity.java
@@ -0,0 +1,42 @@
+package android.example1.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE =
+            "com.example1.android.twoactivity.extra.MESSAGE";
+    private EditText mMessageEditText;
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = (EditText) findViewById(R.id.editText_main);
+        mReplyHeadTextView = (TextView) findViewById(R.id.text_header_reply);
+        mReplyTextView = (TextView) findViewById(R.id.text_message_reply);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void setmMessageEditText(EditText mMessageEditText) {
+        this.mMessageEditText = mMessageEditText;
+    }
+}
